ade72bf4618d90bdda17a0fc9ec9a8f6ba74694b
Naman Arora <naarora@tcd.ie>
2020-07-21 02:14:02 +0530

    Day16

---
+++ Remove_Linked_List_Elements.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (head == nullptr)
+            return head;
+        
+        ListNode *prev, *curr;
+        prev = nullptr;
+        curr = head;
+        
+        while (curr != nullptr && curr->val == val) {
+            prev = curr;
+            curr = curr->next;
+            head = curr;
+        }
+        
+        if (curr == nullptr)
+            return head;
+        
+        while (curr != nullptr) {
+            if (curr->val == val) {
+                prev->next = curr->next;
+                curr = curr->next;
+            }
+            else {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        
+        return head;
+    }
+};
